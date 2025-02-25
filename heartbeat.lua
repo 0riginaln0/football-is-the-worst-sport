9a384a9bf6794048a4743264ea3311b552106c26
Ryzh <gooddmitriy@mail.ru>
2025-02-25 19:21:37 +0300

    heartbeating to the observer server

--- heartbeat.lua
+++ heartbeat.lua
@@ -1,19 +1,27 @@
 require 'lovr.filesystem' -- To get `require` work properly
 local http = require 'http'
+local json = require 'utils.json'
 local lovr = {
     thread = require 'lovr.thread',
     timer = require 'lovr.timer',
 }
 
-local observer_address = 'http://127.0.0.1/hearbeat'
-
+local observer_address = 'http://yourip:yourport/'
+local port = 2
 local counter = 0
 while true do
-    local status, data = http.request('https://zombo.com')
+    local headers = {
+        ["Content-Type"] = "application/json",
+        ["accept"] = "application/json",
+    }
+    local info = json.encode { players_count = counter }
+    local status, data = http.request(
+        observer_address .. "heartbeat?port=" .. port,
+        { headers = headers, data = info })
 
     print('welcome #' .. counter)
     print(status)
     print(data)
     counter = counter + 1
-    lovr.timer.sleep(1)
+    lovr.timer.sleep(2)
 end

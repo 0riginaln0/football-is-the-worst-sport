5a273b05e87781661ec6dec978b115909e75e92a
Ryzh <gooddmitriy@mail.ru>
2025-03-12 18:14:40 +0300

    tab size goes brrrrrrrrrr+1

--- conf.lua
+++ conf.lua
@@ -1,19 +1,19 @@
 ---@diagnostic disable: duplicate-set-field
 function lovr.conf(t)
-   t.modules.headset = false
+    t.modules.headset = false
 
-   t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
-   t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
-   t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
-   t.window.minwidth = 1               -- Minimum window width if the window is resizable (number)
-   t.window.minheight = 1              -- Minimum window height if the window is resizable (number)
-   t.window.display = 1                -- Index of the monitor to show the window in (number)
-   t.window.centered = true            -- Align window on the center of the monitor (boolean)
-   t.window.topmost = false            -- Show window on top (boolean)
-   t.window.borderless = false         -- Remove all border visuals from the window (boolean)
-   t.window.resizable = true           -- Let the window be user-resizable (boolean)
-   t.window.opacity = 1                -- Window opacity value (number)
+    t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
+    t.window.x = nil                   -- The x-coordinate of the window's position in the specified display (number)
+    t.window.y = nil                   -- The y-coordinate of the window's position in the specified display (number)
+    t.window.minwidth = 1              -- Minimum window width if the window is resizable (number)
+    t.window.minheight = 1             -- Minimum window height if the window is resizable (number)
+    t.window.display = 1               -- Index of the monitor to show the window in (number)
+    t.window.centered = true           -- Align window on the center of the monitor (boolean)
+    t.window.topmost = false           -- Show window on top (boolean)
+    t.window.borderless = false        -- Remove all border visuals from the window (boolean)
+    t.window.resizable = true          -- Let the window be user-resizable (boolean)
+    t.window.opacity = 1               -- Window opacity value (number)
 
-   ---@diagnostic disable-next-line: lowercase-global
-   conf = t.window
+    ---@diagnostic disable-next-line: lowercase-global
+    conf = t.window
 end
--- utils/cursor.lua
+++ utils/cursor.lua
@@ -2,50 +2,50 @@ local m = {}
 
 -- next three functions convert mouse coordinate from screen to the 3D position on the ground plane
 local function getWorldFromScreen(pass, cam)
-   local w, h = pass:getDimensions()
-   local clip_from_screen = mat4(-1, -1, 0):scale(2 / w, 2 / h, 1)
-   -- local view_pose = pass:getViewPose(1, mat4(), true):invert()
-   local view_pose = lovr.math.mat4(cam.pose)
-   -- local view_proj = pass:getProjection(1, mat4())
-   local view_proj = lovr.math.mat4(cam.projection)
-   return view_pose:mul(view_proj:invert()):mul(clip_from_screen)
+    local w, h = pass:getDimensions()
+    local clip_from_screen = mat4(-1, -1, 0):scale(2 / w, 2 / h, 1)
+    -- local view_pose = pass:getViewPose(1, mat4(), true):invert()
+    local view_pose = lovr.math.mat4(cam.pose)
+    -- local view_proj = pass:getProjection(1, mat4())
+    local view_proj = lovr.math.mat4(cam.projection)
+    return view_pose:mul(view_proj:invert()):mul(clip_from_screen)
 end
 
 
 local function getRay(world_from_screen, distance)
-   local NEAR_PLANE = 0.01
-   distance = distance or 1e3
-   local ray = {}
-   local x, y = lovr.mouse.getPosition()
-   ray.origin = vec3(world_from_screen:mul(x, y, NEAR_PLANE / NEAR_PLANE))
-   ray.target = vec3(world_from_screen:mul(x, y, NEAR_PLANE / distance))
-   return ray
+    local NEAR_PLANE = 0.01
+    distance = distance or 1e3
+    local ray = {}
+    local x, y = lovr.mouse.getPosition()
+    ray.origin = vec3(world_from_screen:mul(x, y, NEAR_PLANE / NEAR_PLANE))
+    ray.target = vec3(world_from_screen:mul(x, y, NEAR_PLANE / distance))
+    return ray
 end
 
 
 local function mouseOnGround(ray)
-   if ray.origin:distance(ray.target) < 1e-2 then
-      return vec3(0, 0, 0)
-   end
-   local ray_direction = (ray.target - ray.origin):normalize()
-   -- intersect the ray onto ground plane
-   local plane_direction = vec3(0, 1, 0)
-   local dot = ray_direction:dot(plane_direction)
-   if dot == 0 then
-      return vec3(0, 0, 0)
-   end
-   ---@diagnostic disable-next-line: undefined-field
-   local ray_length = (-ray.origin):dot(plane_direction) / dot
-   local hit_spot = ray.origin + ray_direction * ray_length
-   hit_spot.y = 0.0
-   return hit_spot
+    if ray.origin:distance(ray.target) < 1e-2 then
+        return vec3(0, 0, 0)
+    end
+    local ray_direction = (ray.target - ray.origin):normalize()
+    -- intersect the ray onto ground plane
+    local plane_direction = vec3(0, 1, 0)
+    local dot = ray_direction:dot(plane_direction)
+    if dot == 0 then
+        return vec3(0, 0, 0)
+    end
+    ---@diagnostic disable-next-line: undefined-field
+    local ray_length = (-ray.origin):dot(plane_direction) / dot
+    local hit_spot = ray.origin + ray_direction * ray_length
+    hit_spot.y = 0.0
+    return hit_spot
 end
 
 function m.cursorToWorldPoint(pass, cam)
-   local world_from_screen = getWorldFromScreen(pass, cam)
-   local ray = getRay(world_from_screen)
-   local spot = mouseOnGround(ray)
-   return spot
+    local world_from_screen = getWorldFromScreen(pass, cam)
+    local ray = getRay(world_from_screen)
+    local spot = mouseOnGround(ray)
+    return spot
 end
 
 return m
